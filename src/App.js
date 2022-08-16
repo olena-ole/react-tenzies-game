e1e3429ec87e417e66fb16d4add5dc029cd65a7b
Olena <pampam32121@gmail.com>
2022-08-16 04:19:37 +0300

    created Die component and rendered 10 dices manually

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Die from './components/Die';
 
 function App() {
   return (
     <main className="App">
-
+      <div className="dice-wrapper">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
     </main>
   );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,11 @@
+import './die.css';
+
+export default function Die(props) {
+
+    return (
+        <div className="die">
+            {props.value}
+        </div>
+    )
+
+}
